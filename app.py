c2e1f4c7a86e09df4573b15d850bc2832ae3321e
Hagar Mahmoud <93208256+Hagar814@users.noreply.github.com>
2024-03-01 17:14:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,render_template
-
+from flask import Flask, redirect,render_template, url_for
+from form  import RegistrationForm,LoginForm
+import random
 app = Flask(__name__)
-
+app.config["SECRET_KEY"]="290964d444b41b98f8c9bd984b71a7c6"
 news=[
     {"author": "tmohamed@insider.com (Theron Mohamed)",
 "title": "'Magnificent 7' tech stocks are dangerously dominant — and recession's still a real risk, top economist warns",
@@ -22,6 +23,23 @@ def home():
 def about():
     return render_template('about.html',title='About')
 
+@app.route("/register",methods=["GET","POST"])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for("home"))
+    return render_template('register.html',title='register',random=random.random(),form=form)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if(
+            form.email.data=="hagar@gmail.com"
+            and form.password.data=="Hagar@12"
+        ):
+            return redirect(url_for("home"))
+    return render_template('login.html',title='login',random=random.random(),form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
